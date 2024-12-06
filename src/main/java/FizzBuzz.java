df69c0d714e1880ce8a4042ad2ce46e6e5663875
Salaia <puma.hope@yandex.ru>
2024-12-06 18:14:56 +0300

    SQL 1393, 1729

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,11 +5,12 @@ import java.util.List;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        System.out.println(fizzBuzz(15));
+        //System.out.println(fizzBuzz(15));
         // Time is represented to nanosecond precision.
         // For example, the value "2nd October 2007 at 13:45.30.123456789" can be stored in a LocalDateTime.
-        System.out.println(LocalDateTime.now());
-        System.out.println("Over");
+        //System.out.println(LocalDateTime.now());
+        //System.out.println("Over");
+        System.out.println(1+2+" "+1+2+(1+2));
     }
 
     public static List<String> fizzBuzz(int n) {
